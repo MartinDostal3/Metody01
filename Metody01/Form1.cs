e7c929ecf8e28a167b11f53bcbacea53924cb522
dostalm.04 <dostalm.04@A321-08>
2023-01-26 11:31:34 +0100

    Přidejte soubory projektu.

---
+++ Metody01/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metody01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static int SoucetArPosl(int a1, int a2, int n, out int diference, out int posledni)
+        {
+            int soucet = a1;
+            posledni = a1;
+            diference = a2 - a1;
+            
+            
+            for (int i = 0; i < n - 1; i++)
+            {
+                soucet += a1 + (i + 1) * diference;
+                posledni = posledni + diference;
+            }
+            return soucet;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int diference;
+            int posledni;
+            MessageBox.Show("Soucet je: " + SoucetArPosl(5, 7, 6, out diference, out posledni) + " diference je: " + diference + " a posledni clen je: " + posledni);
+
+        }
+    }
+}
---
+++ Metody02/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metody02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+
+
+
+        public static void UlozitCisla(double[] pole, TextBox textBox)
+        {
+            
+            pole = new double[textBox.Lines.Count()];
+            for (int i = 0; i < textBox.Lines.Count(); i++)
+            {
+                pole[i] = double.Parse(textBox.Lines[i]);
+            }
+            
+        }
+
+       public static void ZobrazitPole(ListBox listBox, double[] pole)
+        {
+            listBox.Items.Clear();
+            for (int i = 0; i < pole.Length; i++)
+            {
+                listBox.Items.Add(pole[i]);
+            }
+        }
+
+        public static bool JeRostouci(double[] pole)
+        {
+            for (int i = 1; i < pole.Length; i++)
+            {
+                if (pole[i] <= pole[i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double[] pole = null;
+
+            UlozitCisla(pole, textBox1);
+            ZobrazitPole(listBox1, pole);
+
+            if (JeRostouci(pole))
+            {
+                MessageBox.Show("Pole je rostoucí");
+            }
+            else MessageBox.Show("Pole není rostoucí");
+
+        }
+    }
+}
